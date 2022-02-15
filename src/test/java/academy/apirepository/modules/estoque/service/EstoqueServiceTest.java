29b253645fd9a1d622066be264040e4b9dfefc0d
Luccas Pereira <Luccas.lpn@outlook.com>
2022-02-15 15:40:59 -0300

    Testing FindAll Service

--- src/test/java/academy/apirepository/modules/estoque/service/EstoqueServiceTest.java
+++ src/test/java/academy/apirepository/modules/estoque/service/EstoqueServiceTest.java
@@ -1,9 +1,13 @@
 package academy.apirepository.modules.estoque.service;
 
 import academy.apirepository.modules.estoque.domain.Estoque;
+import academy.apirepository.modules.estoque.enums.PaoEnum;
 import academy.apirepository.modules.estoque.repository.EstoqueRepository;
 import academy.apirepository.modules.estoque.repository.util.EstoqueCreator;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
@@ -34,6 +38,18 @@ class EstoqueServiceTest {
         BDDMockito.when(repository.findAll()).thenReturn(List.of(
                 EstoqueCreator.createdEstoqueTobeSaved()));
 
+    }
+
+    @Test
+    @DisplayName("listAll Return list of when successful")
+    void listAll_ReturnsListAll_WhenSuccessful(){
+        String proteina = EstoqueCreator.createdEstoqueTobeSaved().getProteina();
+        List<Estoque> estoques = service.findAll();
+        Assertions.assertThat(estoques)
+                .isNotNull()
+                .isNotEmpty()
+                .hasSize(1);
+        Assertions.assertThat(estoques.get(0).getProteina()).isEqualTo(proteina);
     }
 
 
