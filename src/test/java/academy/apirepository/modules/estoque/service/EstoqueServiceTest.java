b8bd71661d74f179166c1bd33f1b3c3e12af0e7e
Luccas Pereira <Luccas.lpn@outlook.com>
2022-02-15 15:36:07 -0300

    BDDmockito

--- src/test/java/academy/apirepository/modules/estoque/service/EstoqueServiceTest.java
+++ src/test/java/academy/apirepository/modules/estoque/service/EstoqueServiceTest.java
@@ -1,16 +1,20 @@
 package academy.apirepository.modules.estoque.service;
 
+import academy.apirepository.modules.estoque.domain.Estoque;
 import academy.apirepository.modules.estoque.repository.EstoqueRepository;
+import academy.apirepository.modules.estoque.repository.util.EstoqueCreator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 class EstoqueServiceTest {
-
 
     @InjectMocks
     private EstoqueService service;
@@ -19,5 +23,19 @@ class EstoqueServiceTest {
     private EstoqueRepository repository;
 
 
-    
+
+    @BeforeEach
+    void setUp(){
+        BDDMockito.when(repository.save(ArgumentMatchers.any(Estoque.class)))
+                .thenReturn(EstoqueCreator.createdEstoqueTobeSaved());
+
+        BDDMockito.doNothing().when(repository).delete(ArgumentMatchers.any(Estoque.class));
+
+        BDDMockito.when(repository.findAll()).thenReturn(List.of(
+                EstoqueCreator.createdEstoqueTobeSaved()));
+
+    }
+
+
+
 }
